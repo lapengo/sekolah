7674c240d5e34c85bff1ee188291226d5723a0ad
lapengo <mylapengo@gmail.com>
2020-05-23 05:49:35 +0700

    Fisrt Commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        setlocale(LC_TIME, 'Indonesia');
+        // \Carbon\Carbon::setLocale(config('app.locale'));
+
+        config(['app.locale' => 'id']);
+        \Carbon\Carbon::setLocale('id');
+    }
+}
